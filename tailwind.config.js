45977cb124ab5cb05348b66191e42438cd5c2ff5
david oluwasusi <daviddamilola20@gmail.com>
2022-10-12 22:10:30 +0100

    setup statamic with alpine

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './resources/**/*.antlers.html',
+    './resources/**/*.blade.php',
+    './resources/**/*.vue',
+    './content/**/*.md'
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    colors,
+  ],
+}
